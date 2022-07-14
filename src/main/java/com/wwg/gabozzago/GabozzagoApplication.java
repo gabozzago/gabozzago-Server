8e4e310689c7d8c7dae85540152ffbf4aff0ce4f
shgur7236 <shgurtns7236@naver.com>
2022-07-14 22:29:15 +0900

    test

--- src/main/java/com/wwg/gabozzago/GabozzagoApplication.java
+++ src/main/java/com/wwg/gabozzago/GabozzagoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GabozzagoApplication {
-
+    //test
     public static void main(String[] args) {
         SpringApplication.run(GabozzagoApplication.class, args);
     }
