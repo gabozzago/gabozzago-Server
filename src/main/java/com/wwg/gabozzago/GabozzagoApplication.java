b5a71c2f99031663569cc6076ad4d283872ed584
shgur7236 <shgurtns7236@naver.com>
2022-07-14 21:10:46 +0900

    init

---
+++ src/main/java/com/wwg/gabozzago/GabozzagoApplication.java
@@ -0,0 +1,13 @@
+package com.wwg.gabozzago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabozzagoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GabozzagoApplication.class, args);
+    }
+
+}
