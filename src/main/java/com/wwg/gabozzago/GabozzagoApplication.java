d647e23898f41836202f80ce7e426c470752f884
ssyy0122 <z01039118740@gmail.com>
2022-08-09 22:40:06 +0900

    test

--- src/main/java/com/wwg/gabozzago/GabozzagoApplication.java
+++ src/main/java/com/wwg/gabozzago/GabozzagoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GabozzagoApplication {
+public class ㄹGabozzagoApplication {
     //test
     public static void main(String[] args) {
         SpringApplication.run(GabozzagoApplication.class, args);
