07fc793d611842e5670c20173ef4331ac9211666
shgur7236 <shgurtns7236@naver.com>
2022-08-09 22:51:01 +0900

    Create UserRepository

---
+++ src/main/java/com/wwg/gabozzago/domain/user/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.wwg.gabozzago.domain.user.repository;
+
+import com.wwg.gabozzago.domain.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
