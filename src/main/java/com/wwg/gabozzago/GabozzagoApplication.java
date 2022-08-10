e730466e2be6c2ce57b933f78d66d48ec3525e40
ssyy0122 <z01039118740@gmail.com>
2022-08-10 18:26:07 +0900

    ㄹ발견

--- src/main/java/com/wwg/gabozzago/GabozzagoApplication.java
+++ src/main/java/com/wwg/gabozzago/GabozzagoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ㄹGabozzagoApplication {
+public class GabozzagoApplication {
     //test
     public static void main(String[] args) {
         SpringApplication.run(GabozzagoApplication.class, args);
