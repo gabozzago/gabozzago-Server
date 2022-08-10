cf533f7cf68dc1dcca2066ed84201e0aaccc74f3
ssyy0122 <z01039118740@gmail.com>
2022-08-10 18:22:57 +0900

    PostDto생성

--- src/main/java/com/wwg/gabozzago/domain/post/Dto/PostDto.java
+++ src/main/java/com/wwg/gabozzago/domain/post/Dto/PostDto.java
@@ -1,4 +1,16 @@
 package com.wwg.gabozzago.domain.post.Dto;
 
+import com.wwg.gabozzago.domain.entity.Post;
+import lombok.*;
+
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class PostDto {
+    private String title;
+    private String location;
+    private String post_img;
+    private String content;
 }
