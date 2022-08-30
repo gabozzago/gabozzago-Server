95cfb7b714531297794b6e3d0c80e558993168bc
shgur7236 <shgurtns7236@naver.com>
2022-08-30 10:23:02 +0900

    PostNotFound 예외처리

--- src/main/java/com/wwg/gabozzago/global/error/exception/ErrorCode.java
+++ src/main/java/com/wwg/gabozzago/global/error/exception/ErrorCode.java
@@ -11,9 +11,10 @@ public enum ErrorCode {
     UNAUTHORIZED(401,"Unauthorized"),
     EXPIRED_TOKEN_EXCEPTION(401,"Expired Token"),
     INVALID_TOKEN_EXCEPTION(401,"Invalid Token"),
-    USER_NOT_FOUND(404,"User Not Fount"),
+    USER_NOT_FOUND(404,"User Not Found"),
 
-    UNKNOWN_ERROR(500,"Unknown Error");
+    UNKNOWN_ERROR(500,"Unknown Error"),
+    Post_Not_Found(404,"Post Not Found");
 
     private final int status;
     private final String message;
