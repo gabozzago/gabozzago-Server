6bb0f4cb2268ff1acf0b6030f7d3c005dc542337
ssyy0122 <z01039118740@gmail.com>
2022-08-30 18:06:03 +0900

    코드 손봐주기

--- src/main/java/com/wwg/gabozzago/domain/post/controller/PostApiController.java
+++ src/main/java/com/wwg/gabozzago/domain/post/controller/PostApiController.java
@@ -5,6 +5,7 @@ import com.wwg.gabozzago.domain.post.dto.CreatePostRequestDto;
 import com.wwg.gabozzago.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.processing.FilerException;
@@ -15,14 +16,15 @@ import javax.annotation.processing.FilerException;
 public class PostApiController {
     private final PostService postService;
     //게시물 생성
-    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/create")
-    public void save(@RequestBody CreatePostRequestDto createPostRequestDto) throws FilerException {
+    public ResponseEntity<Void> save(@RequestBody CreatePostRequestDto createPostRequestDto){
             postService.save(createPostRequestDto);
+            return new ResponseEntity<>(HttpStatus.CREATED);
     }
     //게시물 삭제
     @DeleteMapping("/delete/{id}")
-        public void delete(@PathVariable Long id){
+        public ResponseEntity<Void>delete(@PathVariable Long id){
              postService.delete(id);
+             return new ResponseEntity<>(HttpStatus.OK);
     }
 }
