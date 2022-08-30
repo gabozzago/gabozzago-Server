dc72d3d5fa09c2df4d2d8ae2b4e36db4d6b14ec0
ssyy0122 <z01039118740@gmail.com>
2022-08-30 09:11:44 +0900

    PostDelete구현

--- src/main/java/com/wwg/gabozzago/domain/post/service/Impl/PostServiceImpl.java
+++ src/main/java/com/wwg/gabozzago/domain/post/service/Impl/PostServiceImpl.java
@@ -1,9 +1,12 @@
 package com.wwg.gabozzago.domain.post.service.Impl;
 
 import com.wwg.gabozzago.domain.post.dto.CreatePostRequestDto;
+import com.wwg.gabozzago.domain.post.entity.Post;
 import com.wwg.gabozzago.domain.post.repository.PostRepository;
 import com.wwg.gabozzago.domain.post.service.PostService;
+import com.wwg.gabozzago.global.user.exception.UserNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.processing.FilerException;
@@ -17,5 +20,12 @@ public class PostServiceImpl implements PostService {
     @Transactional
     public void save(CreatePostRequestDto createPostRequestDto) throws FilerException {
         postRepository.save(createPostRequestDto.toEntity());
+    }
+
+    @Override
+    public void delete(Long id) {
+        Post post = postRepository.findById(id).orElseThrow(UserNotFoundException::new);
+        postRepository.delete(post);
+
     }
 }
