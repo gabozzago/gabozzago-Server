44912b1a10c53eeb22e5d41c03b7670692885ac2
shgur7236 <shgurtns7236@naver.com>
2022-08-30 14:43:57 +0900

    :sparkles: 좋아요기능 controller부분

--- src/main/java/com/wwg/gabozzago/domain/post/controller/PostApiController.java
+++ src/main/java/com/wwg/gabozzago/domain/post/controller/PostApiController.java
@@ -1,10 +1,12 @@
 package com.wwg.gabozzago.domain.post.controller;
 
 
+import com.wwg.gabozzago.domain.likes.service.LikesService;
 import com.wwg.gabozzago.domain.post.dto.CreatePostRequestDto;
 import com.wwg.gabozzago.domain.post.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.processing.FilerException;
@@ -14,6 +16,7 @@ import javax.annotation.processing.FilerException;
 @RequestMapping("/post")
 public class PostApiController {
     private final PostService postService;
+    private final LikesService likesService;
 
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/create")
@@ -21,4 +24,15 @@ public class PostApiController {
             postService.save(createPostRequestDto);
     }
 
+    @PostMapping("/{postId}/likes")
+    public ResponseEntity<?> likes(@PathVariable Long postId){
+        likesService.likes(postId);
+        return  new ResponseEntity<>("좋아여 성공",HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{postId}/unlikes")
+    public ResponseEntity<?> unlikes(@PathVariable Long postId){
+        likesService.unlikes(postId);
+        return new ResponseEntity<>("좋아여 취소 성공",HttpStatus.OK);
+    }
 }
