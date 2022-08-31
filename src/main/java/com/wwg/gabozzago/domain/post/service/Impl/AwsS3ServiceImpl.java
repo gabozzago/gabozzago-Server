e0d68bd04478108e24cb7905e606e5d203eb7085
ssyy0122 <z01039118740@gmail.com>
2022-08-31 20:03:16 +0900

    Multipart를 전달받아서 파일로전환하는 로직

--- src/main/java/com/wwg/gabozzago/domain/post/service/Impl/AwsS3ServiceImpl.java
+++ src/main/java/com/wwg/gabozzago/domain/post/service/Impl/AwsS3ServiceImpl.java
@@ -1,7 +1,53 @@
 package com.wwg.gabozzago.domain.post.service.Impl;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.wwg.gabozzago.domain.post.service.AwsS3Service;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.server.ResponseStatusException;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class AwsS3ServiceImpl implements AwsS3Service {
+    @Value("${cloud.aws.s3.bucket}")
+    private String bucket;
+
+    private final AmazonS3 amazonS3;
+
+    @Override
+    public List<String> uploadImage(List<MultipartFile> multipartFile) {
+        List<String> fileNameList = new ArrayList<>();
+
+        multipartFile.forEach(file -> {
+            String fileName = createFileName(file.getOriginalFilename());
+            ObjectMetadata objectMetadata = new ObjectMetadata();
+            objectMetadata.setContentLength(file.getSize());
+            objectMetadata.setContentType(file.getContentType());
+
+            try (InputStream inputStream = file.getInputStream()) {
+                amazonS3.putObject(new PutObjectRequest(bucket, fileName, inputStream, objectMetadata)
+                        .withCannedAcl(CannedAccessControlList.PublicRead));
+            } catch (IOException exception) {
+                throw new ResponseStatusException(HttpStatus.INTERNAL_SERVER_ERROR, "이미지업로드 실패");
+            }
+            fileNameList.add(fileName);
+        });
+        return fileNameList;
+    }
+
+    private String createFileName(String originalFilename) {
+
+    }
 
 }
