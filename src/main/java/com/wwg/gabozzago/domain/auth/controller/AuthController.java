2bcfb1bc6e68804d0f29839137b1ad1dc950a7a9
shgur7236 <shgurtns7236@naver.com>
2022-08-31 20:24:02 +0900

    :sparkles: RefreshToken 부분 생성

--- src/main/java/com/wwg/gabozzago/domain/auth/controller/AuthController.java
+++ src/main/java/com/wwg/gabozzago/domain/auth/controller/AuthController.java
@@ -5,14 +5,12 @@ import com.wwg.gabozzago.domain.auth.data.request.LoginRequest;
 import com.wwg.gabozzago.domain.auth.data.response.LoginResponse;
 import com.wwg.gabozzago.domain.auth.data.response.TokenResponse;
 import com.wwg.gabozzago.domain.auth.service.LoginService;
+import com.wwg.gabozzago.domain.auth.service.RefreshTokenService;
 import com.wwg.gabozzago.domain.auth.utils.AuthConverter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthController {
     private final LoginService loginService;
     private final AuthConverter authConverter;
+    private final RefreshTokenService refreshTokenService;
     @PostMapping("/oauth")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest loginRequest){
         LoginDto loginDto = loginService.login(loginRequest);
@@ -27,4 +26,11 @@ public class AuthController {
         TokenResponse tokenResponse = new TokenResponse(loginResponse.getTokenResponse());
         return new ResponseEntity(tokenResponse, HttpStatus.valueOf(loginResponse.getStatus()));
     }
+
+    @PutMapping("/refresh")
+    public ResponseEntity<TokenResponse>refresh(@RequestHeader("Refresh-Token") String refreshToken){
+        TokenResponse tokenResponse = refreshTokenService.refresh(refreshToken);
+        return new ResponseEntity<>(tokenResponse,HttpStatus.OK);
+    }
+
 }
