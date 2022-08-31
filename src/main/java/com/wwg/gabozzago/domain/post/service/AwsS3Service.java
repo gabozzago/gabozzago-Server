af7e39e3ce976f04e13210c8740c536a9a5b2af3
ssyy0122 <z01039118740@gmail.com>
2022-08-31 11:58:50 +0900

    Service File add

---
+++ src/main/java/com/wwg/gabozzago/domain/post/service/AwsS3Service.java
@@ -0,0 +1,5 @@
+package com.wwg.gabozzago.domain.post.service;
+
+public interface AwsS3Service {
+
+}
--- src/main/java/com/wwg/gabozzago/global/config/aws/AwsS3Config.java
+++ src/main/java/com/wwg/gabozzago/global/config/aws/AwsS3Config.java
@@ -28,6 +28,5 @@ public class AwsS3Config {
                 })
                 .build();
     }
-
 }
 
