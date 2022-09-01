db942a8843da71f59eac0be0d750aff6e3064ed8
yoonjibin <gsm07@GSMui-MacBookPro.local>
2022-09-01 22:39:56 +0900

    :sparkles: 핸들러 추가
    DD

--- src/main/java/com/wwg/gabozzago/global/error/handler/GlobalExceptionHandler.java
+++ src/main/java/com/wwg/gabozzago/global/error/handler/GlobalExceptionHandler.java
@@ -3,11 +3,12 @@ package com.wwg.gabozzago.global.error.handler;
 import com.wwg.gabozzago.global.error.ErrorCode;
 import com.wwg.gabozzago.global.error.ErrorResponse;
 import com.wwg.gabozzago.global.error.exception.ExpiredTokenException;
+import com.wwg.gabozzago.global.error.exception.InvalidTokenException;
+import com.wwg.gabozzago.global.error.exception.PostNotFoundException;
+import com.wwg.gabozzago.global.error.exception.UserNotFoundException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.BindException;
-import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,16 +23,27 @@ import java.util.Map;
 public class GlobalExceptionHandler {
     @ExceptionHandler(ExpiredTokenException.class)
     public ResponseEntity<ErrorResponse> ExpiredTokenException(HttpServletRequest request,ExpiredTokenException e){
+        printException(request,e.getErrorCode());
         ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
         return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatus()));
     }
-    @ExceptionHandler(BindException.class)
-    public ResponseEntity<?>BindException(BindException e){
-        Map<String,String> errorMap = new HashMap<>();
-        for(FieldError error : e.getFieldErrors()){
-            errorMap.put(error.getField(),error.getDefaultMessage());
-        }
-        return new ResponseEntity<>(errorMap,HttpStatus.BAD_REQUEST);
+    @ExceptionHandler(InvalidTokenException.class)
+    public ResponseEntity<ErrorResponse> InvalidTokenException(HttpServletRequest request,InvalidTokenException e){
+        printException(request,e.getErrorCode());
+        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
+        return new ResponseEntity<>(errorResponse,HttpStatus.valueOf(errorResponse.getStatus()));
+    }
+    @ExceptionHandler(PostNotFoundException.class)
+    public ResponseEntity<ErrorResponse> PostNotFoundException(HttpServletRequest request,PostNotFoundException e){
+        printException(request,e.getErrorCode());
+        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatus()));
+    }
+    @ExceptionHandler(UserNotFoundException.class)
+    public ResponseEntity<ErrorResponse> UserNotFoundException(HttpServletRequest request,UserNotFoundException e){
+        printException(request,e.getErrorCode());
+        ErrorResponse errorResponse = new ErrorResponse(e.getErrorCode());
+        return new ResponseEntity<>(errorResponse, HttpStatus.valueOf(errorResponse.getStatus()));
     }
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<?>ConstraintViolationException(ConstraintViolationException e){
