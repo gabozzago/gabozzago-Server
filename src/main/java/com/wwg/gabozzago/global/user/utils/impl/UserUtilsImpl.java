8d90d8d63c542d1e0d3ddeb981152482a3dbd1e7
ssyy0122 <z01039118740@gmail.com>
2022-09-01 09:38:24 +0900

    test

--- src/main/java/com/wwg/gabozzago/global/user/utils/impl/UserUtilsImpl.java
+++ src/main/java/com/wwg/gabozzago/global/user/utils/impl/UserUtilsImpl.java
@@ -9,13 +9,8 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 
 @Service
-<<<<<<< HEAD:src/main/java/com/wwg/gabozzago/global/user/utils/impl/userUtilsImpl.java
-@RequiredArgsConstructor //TODO 지인호 | Java에서는 Class 이름을 Camel Case 로 작명하는것을 권장하고있어요. "UserUtilsImpl" 은 어떠신가요??
-public class userUtilsImpl implements userUtils {
-=======
 @RequiredArgsConstructor
 public class UserUtilsImpl implements UserUtils {
->>>>>>> c725a2f591b8214f81c0e82afe5af9b982d6e7d8:src/main/java/com/wwg/gabozzago/global/user/utils/impl/UserUtilsImpl.java
     private final UserRepository userRepository;
 
     @Override
