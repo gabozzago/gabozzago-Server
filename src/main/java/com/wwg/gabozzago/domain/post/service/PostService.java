66574ef5484d2e4c190b79342ac4a007f4bb2469
ssyy0122 <z01039118740@gmail.com>
2022-09-02 17:00:12 +0900

    test

--- src/main/java/com/wwg/gabozzago/domain/post/service/PostService.java
+++ src/main/java/com/wwg/gabozzago/domain/post/service/PostService.java
@@ -3,6 +3,9 @@ package com.wwg.gabozzago.domain.post.service;
 import com.wwg.gabozzago.domain.post.data.request.CreatePostRequestDto;
 
 public interface PostService {
+     //게시물 등록
+     void save(CreatePostRequestDto createPostRequestDto);
+     void delete(Long id);
      //게시물 생성
      void save(CreatePostRequestDto createPostRequestDto);
      //게시물 삭제
