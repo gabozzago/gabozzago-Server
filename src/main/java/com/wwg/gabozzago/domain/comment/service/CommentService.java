eeba89ed01e5179f4abe4efd4bc421ba03414bfc
shgur7236 <shgurtns7236@naver.com>
2022-09-06 00:13:35 +0900

    :sparkles: addCommentService

--- src/main/java/com/wwg/gabozzago/domain/comment/service/CommentService.java
+++ src/main/java/com/wwg/gabozzago/domain/comment/service/CommentService.java
@@ -1,4 +1,7 @@
 package com.wwg.gabozzago.domain.comment.service;
 
+import com.wwg.gabozzago.domain.comment.entity.Comment;
+
 public interface CommentService {
+    Comment addComment(String comment, Long postId);
 }
