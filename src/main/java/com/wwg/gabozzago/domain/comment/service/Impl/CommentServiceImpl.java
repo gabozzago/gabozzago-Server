2e6c9daa461e0bc10c9b3e454ebb02c260760415
shgur7236 <shgurtns7236@naver.com>
2022-09-06 20:40:58 +0900

    :recycle: code 수정

--- src/main/java/com/wwg/gabozzago/domain/comment/service/Impl/CommentServiceImpl.java
+++ src/main/java/com/wwg/gabozzago/domain/comment/service/Impl/CommentServiceImpl.java
@@ -1,15 +1,19 @@
 package com.wwg.gabozzago.domain.comment.service.Impl;
 
+import com.wwg.gabozzago.domain.comment.data.request.CommentRequestDto;
 import com.wwg.gabozzago.domain.comment.entity.Comment;
 import com.wwg.gabozzago.domain.comment.repository.CommentRepository;
 import com.wwg.gabozzago.domain.comment.service.CommentService;
 import com.wwg.gabozzago.domain.post.entity.Post;
 import com.wwg.gabozzago.domain.user.entity.User;
 import com.wwg.gabozzago.domain.user.post.repository.PostRepository;
+import com.wwg.gabozzago.global.error.ErrorCode;
+import com.wwg.gabozzago.global.error.exception.PostNotFoundException;
 import com.wwg.gabozzago.global.user.utils.UserUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
 
 @RequiredArgsConstructor
 @Service
@@ -21,15 +25,16 @@ public class CommentServiceImpl implements CommentService {
 
     @Transactional
     @Override
-    public Comment addComment(String content, Long postId) {
+    public Long addComment(Long postId, CommentRequestDto commentRequestDto) {
         User user = userUtils.getCurrentUser();
-        Post post = postRepository.findById(postId).get();
-        Comment comment = Comment.builder()
-                .content(content)
-                .post(post)
-                .user(user)
-                .build();
+        Post post = postRepository.findById(postId).orElseThrow(()-> new PostNotFoundException(ErrorCode.POST_NOT_FOUND));
 
-        return commentRepository.save(comment);
+        commentRequestDto.setUser(user);
+        commentRequestDto.setPost(post);
+
+        Comment comment = commentRequestDto.toEntity();
+        commentRepository.save(comment);
+
+        return commentRequestDto.getId();
     }
 }
