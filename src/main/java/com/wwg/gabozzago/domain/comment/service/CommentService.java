3e1e74bd005e65ea3202c54507e0aa21404ed58d
shgur7236 <shgurtns7236@naver.com>
2022-09-06 20:40:17 +0900

    매개변수 수정

--- src/main/java/com/wwg/gabozzago/domain/comment/service/CommentService.java
+++ src/main/java/com/wwg/gabozzago/domain/comment/service/CommentService.java
@@ -1,7 +1,7 @@
 package com.wwg.gabozzago.domain.comment.service;
 
-import com.wwg.gabozzago.domain.comment.entity.Comment;
+import com.wwg.gabozzago.domain.comment.data.request.CommentRequestDto;
 
 public interface CommentService {
-    Comment addComment(String comment, Long postId);
+    Long addComment(Long id, CommentRequestDto commentRequestDto);
 }
