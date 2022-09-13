fe56a112cb4a2cc2a94131689db72f485617896d
ssyy0122 <z01039118740@gmail.com>
2022-09-13 20:39:42 +0900

    들여쓰기

--- src/main/java/com/wwg/gabozzago/domain/post/service/Impl/PostServiceImpl.java
+++ src/main/java/com/wwg/gabozzago/domain/post/service/Impl/PostServiceImpl.java
@@ -24,37 +24,40 @@ public class PostServiceImpl implements PostService {
     private final PostRepository postRepository;
     private final UserUtils userUtils;
     private final LikesRepository likesRepository;
+
     //게시물 생성
     @Override
     @Transactional
-    public void save(CreatePostRequestDto createPostRequestDto){
+    public void save(CreatePostRequestDto createPostRequestDto) {
         User userInfo = userUtils.getCurrentUser();
         Post post = createPostRequestDto.toEntity(userInfo);
         postRepository.save(post);
     }
+
     //게시물 수정
     @Override
     public void update(Long id, PostUpdateResponse dto) {
         Post post = postRepository.findById(id).orElseThrow(() ->
                 new PostNotFoundException(ErrorCode.POST_NOT_FOUND));
-                post.update(dto.getContent(),dto.getTitle(),dto.getPostImg());
+        post.update(dto.getContent(), dto.getTitle(), dto.getPostImg());
     }
 
     //게시물 삭제
     @Override
     public void delete(Long id) {
-        Post post = postRepository.findById(id).orElseThrow(()->new
+        Post post = postRepository.findById(id).orElseThrow(() -> new
                 PostNotFoundException(ErrorCode.POST_NOT_FOUND));
-            postRepository.delete(post);
+        postRepository.delete(post);
     }
 
     //메인 페이지
     @Override
-    public MainPageResponse getMainPage(){
+    public MainPageResponse getMainPage() {
         List<PostResponse> postResponseList = findAllPostInfo();
         return new MainPageResponse(postResponseList);
     }
-    private List<PostResponse> findAllPostInfo(){
+
+    private List<PostResponse> findAllPostInfo() {
         List<PostResponse> list = new ArrayList<>();
         User currentUser = userUtils.getCurrentUser();
         postRepository.findAll().forEach(post -> {
@@ -63,15 +66,15 @@ public class PostServiceImpl implements PostService {
             String address = post.getLocation();
             String postImg = post.getPostImg();
             post.getLikesList().forEach(likes -> {
-                if(Objects.equals(likes.getUser().getEmail(), currentUser.getEmail())) post.updateLikeState(true);
+                if (Objects.equals(likes.getUser().getEmail(), currentUser.getEmail())) post.updateLikeState(true);
             });
-            list.add(new PostResponse(postId,title,address,postImg,post.isLikesState()));
-       });
+            list.add(new PostResponse(postId, title, address, postImg, post.isLikesState()));
+        });
         return list;
     }
 
     @Override
-    public LikedPostListResponse getLikedPostList(){
+    public LikedPostListResponse getLikedPostList() {
         List<LikedPostResponse> likedPostResponseList = findAllLikedPostInfo();
         return new LikedPostListResponse(likedPostResponseList);
     }
@@ -80,13 +83,13 @@ public class PostServiceImpl implements PostService {
         List<LikedPostResponse> list = new ArrayList<>();
         User currentUser = userUtils.getCurrentUser();
         likesRepository.findAll().forEach(likes -> {
-            if(likes.getUser() == currentUser){
+            if (likes.getUser() == currentUser) {
                 String userName = likes.getUser().getName();
                 String userImg = likes.getUser().getUserImg();
                 String title = likes.getPost().getTitle();
                 String location = likes.getPost().getLocation();
                 String postImg = likes.getPost().getPostImg();
-                list.add(new LikedPostResponse(userName,userImg,title,location,postImg,true));
+                list.add(new LikedPostResponse(userName, userImg, title, location, postImg, true));
             }
         });
         return list;
