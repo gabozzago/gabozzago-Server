ba2fd81a51678631a8bb9e8b8b48b6c33c11f5ec
shgur7236 <shgurtns7236@naver.com>
2022-09-14 20:45:39 +0900

    @Repository 삭제

--- src/main/java/com/wwg/gabozzago/domain/comment/repository/CommentRepository.java
+++ src/main/java/com/wwg/gabozzago/domain/comment/repository/CommentRepository.java
@@ -3,8 +3,6 @@ package com.wwg.gabozzago.domain.comment.repository;
 
 import com.wwg.gabozzago.domain.comment.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 }
